a7c379dfadfb61ae845e091c676f32588ec458de
Gabe Barrera <gsbarrera1@gmail.com>
2019-03-15 23:07:26 -0700

    map functions

--- mapscript.js
+++ mapscript.js
@@ -1,27 +1,45 @@
+var coords = [];
+
 function myMap()
 {
-    var lat = 0;
-    var lon = 0;
+    var exists = false;
+
     myCenter=new google.maps.LatLng(32.77663093727863, -117.06921800036321);
     var mapOptions= {
-    center:myCenter,
-    zoom:12, scrollwheel: true, draggable: true,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+        center:myCenter,
+        zoom:12, scrollwheel: true, draggable: true,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
 
-    var marker = new google.maps.Marker({
-    position: myCenter,
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29),
-    draggable: true
+    // google.maps.event.addListener(marker, 'dragend', function() {
+    //     coords[0] = marker.getPosition().lat();
+    //     coords[1] = marker.getPosition().lng();
+    //     console.log(coords[0],coords[1]);
+    // })
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
     });
-
-    google.maps.event.addListener(marker, 'dragend', function() {
-    lat = marker.getPosition().lat();
-    lon = marker.getPosition().lng();
-    })
-
-    console.log(lat,lon);
-    marker.setMap(map);
+    
+    function placeMarker(location) {
+        if(!exists){
+            let marker = new google.maps.Marker({
+                position: location, 
+                map: map,
+                draggable: true
+            });
+            coords[0] = location.lat();
+            coords[1] = location.lng();
+            google.maps.event.addListener(marker, 'dragend', function() {
+                coords[0] = marker.getPosition().lat();
+                coords[1] = marker.getPosition().lng();
+                console.log(coords[0],coords[1]);
+            });
+            console.log(coords);
+            exists = true;
+        }
+    }
+    
+    // marker.setMap(map);
 }
